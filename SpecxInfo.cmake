02f4e00db5218f1568b6ac08b19f8c40d1319d5d
Berenger Bramas <Berenger.Bramas@inria.fr>
2024-02-01 14:37:33 +0100

    add config files

---
+++ SpecxInfo.cmake
@@ -0,0 +1,5 @@
+
+set(SPECX_CXX_FLAGS  -std=c++17  -funroll-loops -fpic -Wextra -Wnon-virtual-dtor -Wshadow -Wpointer-arith -Wcast-qual -Wall -pedantic -Woverloaded-virtual -Wpointer-arith -Wcast-qual -Wno-error -Wconversion -Wno-sign-conversion -m64 -Wold-style-cast)
+set(SPECX_BINARY_DIR  /home/berenger/Projects/specx)
+set(SPECX_SOURCE_DIR  /home/berenger/Projects/specx)
+set(SPECX_INCLUDE_DIR  )
